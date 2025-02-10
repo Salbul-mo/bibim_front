59583fdbb3aec4db3cdee05035eec6c01dbf32f0
DESKTOP-U7D63L1\MO <salbul.mo91@gmail.com>
2025-02-10 13:32:48 +0900

    fix

--- src/providers/auth-provider.tsx
+++ src/providers/auth-provider.tsx
@@ -4,19 +4,12 @@ import { useEffect } from "react";
 import { useAuthStore } from "@/features/auth/store/auth.store";
 import { usePathname, useRouter } from "next/navigation";
 
-const PUBLIC_PATHS = ["/login", "/signup"];
+const PUBLIC_PATHS = ["/login", "/signup", "/dashboard"];
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
 	const { isAuthenticated } = useAuthStore();
 	const pathname = usePathname();
 	const router = useRouter();
-
-	useEffect(() => {
-		const publicPaths = ["/login"];
-		if (!publicPaths.includes(pathname) && !isAuthenticated) {
-			router.push("/login");
-		}
-	}, [isAuthenticated, pathname, router]);
 
 	return <>{children}</>;
 }
